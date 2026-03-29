856e5dc2643863fec1309d098d5979cd8fd96e7b
David <dkordos@etfos.hr>
2026-03-29 18:58:48 +0200

    Count cars with specific transmission and horsepower
    Filter cars with automatic transmission and more than 100 horsepower.

--- LV3/zadatak1.6.py
+++ LV3/zadatak1.6.py
@@ -3,5 +3,6 @@ import numpy as np
 
 
 mtcars = pd.read_csv('mtcars.csv')
+# automobili s automatskim mjenjačem i snagom preko 100 konjskih snaga
 rezultat = mtcars[(mtcars['am'] == 0) & (mtcars['cyl'] == 6) & (mtcars['hp'] > 100)].shape[0] #shape broji koliko ih ima koji zadovoljavaju uvijet
 print(rezultat)
