52459ff681d78c1d0e3653da14506f44b25ba0ab
David <dkordos@etfos.hr>
2026-03-29 19:11:04 +0200

    Update zadatak2.3.py

--- LV3/zadatak2.3.py
+++ LV3/zadatak2.3.py
@@ -2,14 +2,13 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 mtcars = pd.read_csv('mtcars.csv')
 
-sns.barplot(x='am', y='mpg', data=mtcars, ci=None, palette='Set2') #ci uklanja prikaz intervala pouzdanovsti
+# imaju li automobili s ručnim mjenjačem veću potrošnju od automobila s automatskim mjenjačem
+sns.boxplot(x='am', y='mpg', data=mtcars)
 
-# Naslovi i oznake
-plt.title('Prosječna potrošnja automobila po broju cilindara')
-plt.xlabel('Vrsta mjenjaca')
-plt.ylabel('Potrošnja (mpg)')
+plt.title('Potrosnja ovisno o mjenjacu')
+plt.xlabel('0=automatski, 1=rucni')
+plt.ylabel('potrosnja (mpg)')
 
 plt.show()
