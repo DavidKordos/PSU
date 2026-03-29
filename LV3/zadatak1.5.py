e288de740347f7ce3aea359e6a472b64a5afd54b
David <dkordos@etfos.hr>
2026-03-29 18:53:42 +0200

    Add script to analyze mtcars dataset by transmission

---
+++ LV3/zadatak1.5.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+mtcars = pd.read_csv('mtcars.csv')
+
+rucni = mtcars[mtcars['am'] == 1]
+automatski = mtcars[mtcars['am'] == 0]
+
+print("Rucni:", len(rucni))
+print("Automatski:", len(automatski))
